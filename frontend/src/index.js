646027e9d8be2dd8807d424b28271b24e63eb8ee
CosmoGad <13danilnovikov37@gmail.com>
2024-07-23 11:42:21 +0200

    Updated video sources and fixed project structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('Root element not found');
+}
