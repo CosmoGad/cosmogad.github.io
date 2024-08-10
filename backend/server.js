edb46f7619c8128f3988c77d8b992c51d0d1c138
CosmoGad <13danilnovikov37@gmail.com>
2024-08-10 16:40:27 +0200

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,9 @@ app.use(cors({
     }
     return callback(null, true);
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
 app.use((req, res, next) => {
@@ -33,9 +35,13 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
   next();
 });
 
@@ -44,6 +50,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   console.log('Request received:', req.method, req.url);
   console.log('Request headers:', JSON.stringify(req.headers, null, 2));
+  console.log('Response headers:', JSON.stringify(res.getHeaders(), null, 2));
   next();
 });
 
